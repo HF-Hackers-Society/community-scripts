e30f14829a752eb51ebaa4ccfcbbae89757b8304
T145 <T145@protonmail.com>
2024-01-12 13:43:13 -0500

    Organized tests

--- tests/diffusers/test_diffuser_pipelines.py
+++ tests/diffusers/test_diffuser_pipelines.py
@@ -1,4 +1,5 @@
 from hf_community_scripts.diffusers.loaders.sdxl_loader import load_pipeline
 
-if __name__ == '__main__':
-	pipe = load_pipeline()
+
+def test_answer():
+	load_pipeline()
--- tests/general/test_onnx.py
+++ tests/general/test_onnx.py
@@ -7,5 +7,5 @@ def test_answer():
 	assert 'CUDAExecutionProvider' in get_available_providers(), 'ONNX Runtime GPU provider not found. Make sure onnxruntime-gpu is installed and onnxruntime is uninstalled.'
 	assert 'GPU' == get_device()
 
-	# asser version due to bug in 1.11.1
+	# assert version due to bug in 1.11.1
 	assert onnxruntime.__version__ > '1.11.1', 'You need a newer version of ONNX Runtime!'
