4403cf17a91f83572cacb26be1b6040cdbb68a5d
T145 <T145@protonmail.com>
2024-01-18 16:50:32 -0500

    & the tests

--- tests/diffusers/test_diffuser_pipelines.py
+++ tests/diffusers/test_diffuser_pipelines.py
@@ -1,14 +1,14 @@
 import pytest
-from hf_community_scripts.diffusers.loaders.sdxl_loader import load_pipeline
+from hf_community_scripts.diffusers.loaders.sdxl_loader import load_single_gpu_pipeline
 
 
 def test_sdxl():
-	load_pipeline()
+	load_single_gpu_pipeline()
 
 
 def test_sdxl_freeu():
 	with pytest.raises(Exception) as e_info:
-		load_pipeline(
+		load_single_gpu_pipeline(
 			do_freeu = {
 				's3': 1.0
 			}
